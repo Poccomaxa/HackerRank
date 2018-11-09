c5e27abd1cbe708b1fd27b5bc86f67ca75fe75b5
a_lebedev <a_lebedev@wargaming.net>
2018-11-09 10:33:00 +0300

    Working small variant

--- main.cpp
+++ main.cpp
@@ -2,26 +2,102 @@
 
 using namespace std;
 
-// Complete the steadyGene function below.
-int steadyGene(string gene) {
-	return 0;
+// Complete the morganAndString function below.
+string morganAndString(string a, string b) {
+	string res;
+	int i = 0;
+	int j = 0;
+	while (i < a.size() && j < b.size())
+	{
+		if (a[i] < b[j])
+		{
+			res.push_back(a[i]);
+			++i;
+		}
+		else if (a[i] > b[j])
+		{
+			res.push_back(b[j]);
+			++j;
+		}
+		else
+		{
+			int ii = i + 1;
+			int jj = j + 1;
+			while (ii < a.size() && jj < b.size() && a[ii] == b[jj])
+			{
+				++ii;
+				++jj;
+			}
 
+			if (ii < a.size() && jj < b.size())
+			{
+				if (a[ii] < b[jj])
+				{
+					res.push_back(a[i]);
+					++i;
+				}
+				else
+				{
+					res.push_back(b[j]);
+					++j;
+				}
+			}
+			else if (ii < a.size())
+			{
+				if (a[ii] < b[j])
+				{
+					res.push_back(a[i]);
+					++i;
+				}
+				else
+				{
+					res.push_back(b[j]);
+					++j;
+				}
+			}
+			else if (jj < b.size())
+			{
+				if (b[jj] < a[i])
+				{
+					res.push_back(b[j]);
+					++j;
+				}
+				else
+				{
+					res.push_back(a[i]);
+					++i;
+				}
+			}
+			else
+			{
+				res.push_back(a[i]);
+				++i;
+			}
+		}
+	}
+
+	return res + a.substr(i, a.size()) + b.substr(j, b.size());
 }
 
 int main()
 {
 	ofstream fout(getenv("OUTPUT_PATH"));
 
-	int n;
-	cin >> n;
+	int t;
+	cin >> t;
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-	string gene;
-	getline(cin, gene);
+	for (int t_itr = 0; t_itr < t; t_itr++) {
+		string a;
+		getline(cin, a);
 
-	int result = steadyGene(gene);
+		string b;
+		getline(cin, b);
 
-	fout << result << "\n";
+		string result = morganAndString(a, b);
+
+		fout << result << "\n";
+	}
 
 	fout.close();
 
